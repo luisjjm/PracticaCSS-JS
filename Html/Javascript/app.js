5ae83720924d7872ca43d1deb35f5cd11280f1b7
luisjjm <ljaumemellado@cifpfbmoll.eu>
2026-03-20 17:52:59 +0100

    cambios en la página web

--- Html/Javascript/app.js
+++ Html/Javascript/app.js
@@ -1,3 +1,4 @@
+// función para actualizar el reloj
 function actualizarReloj(){
     const ahora = new Date();
     const fecha = ahora.toLocaleDateString();
@@ -6,6 +7,14 @@ function actualizarReloj(){
     document.getElementById("reloj").textContent = `${fecha} ${hora}`;
 }
 
+// Repite la función consecutivamente 
 setInterval(actualizarReloj, 1000);
 
 actualizarReloj();
+
+// Añade contenido a la sección principal
+fetch("Pages/contenidoArticulos.html")
+.then(res => res.text())
+.then(html => {
+    document.getElementById("articulos").innerHTML = html;
+})
