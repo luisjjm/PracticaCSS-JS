de04e8d5fdf4f42ced8845c8b637cae8310f4bad
ariesjm64 <ljacobojm64@gmail.com>
2026-04-06 21:21:02 +0200

    script Publicaciones

--- Html/Javascript/app.js
+++ Html/Javascript/app.js
@@ -12,7 +12,7 @@ setInterval(actualizarReloj, 1000);
 
 actualizarReloj();
 
-// Añade contenido a la sección principal
+// Artículos de la sección principal
 fetch("Pages/contenidoArticulos.html")
 .then(res => res.text())
 .then(html => {
@@ -22,12 +22,56 @@ fetch("Pages/contenidoArticulos.html")
 // const form = document.forms["loginForm"];
 const form = document.getElementById("form_loggin");
 const mensaje = document.querySelector("#mensaje");
-// debugger;
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    debugger;
     const usuario = form.user.value;
     const contraseña = form.pass.value;
 
-    mensaje.textContent += 'Bienvenido, ' + usuario + '.'
+    mensaje.textContent = 'Bienvenido, ' + usuario + '.';
 });
+
+//Publicaciones
+    const formPublic = document.getElementById("form_public");
+
+    formPublic.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        const titulo = formPublic.titulo.value;
+        const cuerpo = formPublic.cuerpo.value;
+        const imagen = formPublic.imagen.files[0];
+
+        if (!titulo || !cuerpo || !imagen){
+            alert("Por favor rellene titulo, cuerpo y imagen al publicar")
+            return
+        }
+
+        const reader = new FileReader();
+            reader.onload = function(e) {
+                const src = e.target.result;
+
+                const article = document.createElement("article");
+
+                article.innerHTML = `
+                        <div id="contenido">
+                            <div>
+                                <h3>${titulo}</h3>
+                            </div>
+                            <div id="contenido_texto">
+                                <p>${cuerpo}</p>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img id="img_articulo" src="${src}">
+                        </div>
+                `;
+
+                articulos.appendChild(article);
+                formPublic.reset();
+            };
+
+            if (imagen) {
+                reader.readAsDataURL(imagen);
+            }
+
+    });
--- README.md
+++ README.md
@@ -3,8 +3,6 @@ Practica de desarrollo de una página web HTML con CSS y Javascript.
 
 Idea principal: un blog con estilo retro.
 
-El proyecto actualmente cuenta con la siguiente rama de directorios:
-
 El proyecto acutalment ecuenta con 7 directorios:
 
 Html. Engloba el proyecto
@@ -16,3 +14,32 @@ Media. Es donde se ubicará el contenido multimedia, ejemplo: imagenes, videos, 
 Pages. Las páginas enlazadas al index
 
 Styles. Los css utilizados en las páginas.
+
+CSS
+Estilo sencillo y retro. Insipirado en las primeras páginas web estaticas donde todo esta contenido por bloques delimitados. 
+Los colores calidos y márrones son la personalidad del blog.
+
+Una fuente de inspiración han sido las páginas de neocities.
+Gran parte del tiempo dedicado ha sido al CSS, debido a que me entretengo probando formas propias de presentar el contenido.
+
+JS
+Como blog, las funcionalidades en las que me he interesado són el inicio de sesión de un usuario y las publicaciones.
+He añadido un pequeño detalle como un texto dinamico que marca la fecha actual.
+
+Detalles que podria añadir en un futuro es que los datos ingresados se guarden de manera local y mantenerlos aunque se recargue la página.
+
+IA
+Se ha utilizado IA para varias funciones que desconocia, como en el reloj:
+toLocaleDateString();
+toLocaleTimeString();
+setInterval();
+
+Estas funciones no se han empleado en clase, pero son necesarias para que el reloj vaya marcando la hora.
+
+Empleo de fecth();
+Se me ocurrió que seria buena idea que el contenido principal estuviera guardado en otra hoja Html despejando el index.html.
+Pero de nuevo es una función que no empleamos en clase y que investigue utilizando la IA.
+
+FileReader();
+Queria que el usuario pudiera publicar imagenes, resulta que necesita esta función que transforma la imagen en binario a base64,
+para que pueda ser interpretado como contenido src.
